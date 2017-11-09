2b0e491d9d302b59c32217a285086503a249139f
Alex Shumilov <ashumilov@pivotal.io>
2017-11-09 09:25:13 -0500

    Add producer sample test

--- producer/src/test/java/io/microsamples/metamorphosis/producer/ProducerApplicationTests.java
+++ producer/src/test/java/io/microsamples/metamorphosis/producer/ProducerApplicationTests.java
@@ -1,18 +1,39 @@
 package io.microsamples.metamorphosis.producer;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.messaging.Message;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.BlockingQueue;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.cloud.stream.test.matcher.MessageQueueMatcher.receivesPayloadThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@Ignore
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@DirtiesContext
 public class ProducerApplicationTests {
 
+
+	@Autowired
+	private Source channels;
+
+	@Autowired
+	private MessageCollector collector;
+
 	@Test
-	public void contextLoads() {
+	public void testMessages() {
+		BlockingQueue<Message<?>> messages = this.collector.forChannel(channels.output());
+
+		assertThat(messages, receivesPayloadThat(containsString("Hi There")));
+
 	}
 
 }
