172d8767e0a79aaff2aa65af6251ee9df818cd5c
Alex Shumilov <ashumilov@pivotal.io>
2017-11-10 12:25:17 -0500

    Add test placeholder

---
+++ consumer/src/test/java/io/microsamples/metamorphosis/consumer/ConsumerTests.java
@@ -0,0 +1,17 @@
+package io.microsamples.metamorphosis.consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//todo implement this
+public class ConsumerTests {
+
+
+	@Test
+	public void should_process_received_message() {
+
+	}
+
+}
